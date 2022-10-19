e4f36e2fc322b77d0b3d38782d00a96e9e6de1e4
Chad Cooper <chad.cooper@e-gineering.com>
2022-10-19 11:54:01 -0400

    Create Sign in with GitHub button

--- src/lib/Auth.svelte
+++ src/lib/Auth.svelte
@@ -9,7 +9,7 @@
   let signInEmail: string = ''
   let signInPassword: string = ''
 
-  const handleSignIn = async () => {
+  const handleSignInWithPassword = async () => {
     try {
       loadingSignIn = true
 
@@ -26,13 +26,48 @@
       loadingSignIn = false
     }
   }
+
+  const handleSignInWithOAuth = async () => {
+    try {
+      loadingSignIn = true
+
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'github'
+      })
+
+      if (error) throw error
+      console.log(`Logged in user: ${JSON.stringify(data, undefined, 2)}`)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      loadingSignIn = false
+    }
+  }
 </script>
 
 <div class="row flex-center flex">
   <div class="col-6 form-widget" aria-live="polite">
     <h1 class="header">Hacktoberfest Tracker</h1>
+    <p class="description">Sign in using your GitHub account</p>
+    <div>
+      <button
+        type="button"
+        class="button social-button"
+        aria-live="polite"
+        disabled={loadingSignIn}
+        on:click={handleSignInWithOAuth}
+      >
+        <span>
+          <img src="/github.svg" alt="GitHub Icon" />
+        </span>
+        <span>{loadingSignIn ? 'Loading' : 'Sign In with GitHub'}</span>
+      </button>
+    </div>
+    <div role="separator" class="divider">
+      <span class="">or</span>
+    </div>
     <p class="description">Sign in using your email and password below</p>
-    <form class="form-widget" on:submit|preventDefault={handleSignIn}>
+    <form class="form-widget" on:submit|preventDefault={handleSignInWithPassword}>
       <div>
         <label for="email">Email</label>
         <input
@@ -68,3 +103,46 @@
     <p class="description">Don't have an account? Sign up <a href="/signup" use:link>here</a>.</p>
   </div>
 </div>
+
+<style>
+  .social-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    gap: 1rem;
+  }
+
+  .divider {
+    color: rgb(187, 187, 187);
+    margin: 1rem 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    white-space: nowrap;
+    font-size: 0.9rem;
+  }
+
+  .divider::before {
+    border-bottom-style: solid;
+    border-bottom-width: 1px;
+    content: '';
+    position: relative;
+    display: inline-block;
+    width: 50%;
+  }
+
+  .divider span {
+    margin: 1rem;
+  }
+
+  .divider::after {
+    border-bottom-style: solid;
+    border-bottom-width: 1px;
+    content: '';
+    position: relative;
+    display: inline-block;
+    width: 50%;
+  }
+
+</style>
